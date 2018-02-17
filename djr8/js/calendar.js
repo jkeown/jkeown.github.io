a3c7f89da54fda900d92992c1efe92a8e2e19cb5
jared keown <jd.keown@yahoo.com>
2018-02-16 18:36:47 -0800

    change calendar again

---
+++ djr8/js/calendar.js
@@ -0,0 +1,9 @@
+let date = '4/1/18';
+let venue = 'Yo Momma\'s house';
+let time = 'All the time';
+
+document.querySelector('.djevent').innerHTML = `
+<td>${date}</td>
+<td>${venue}</td>
+<td>${time}</td>
+`
