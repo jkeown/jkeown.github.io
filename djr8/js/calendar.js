51ccb31af57eab3df62fb952ed21b39f7396c2b6
jared keown <jd.keown@yahoo.com>
2018-03-18 19:41:43 -0700

    djr8 rework - close to final

--- djr8/js/calendar.js
+++ djr8/js/calendar.js
@@ -1,9 +1,18 @@
-let date = '4/1/18';
-let venue = 'Yo Momma\'s house';
-let time = 'All the time';
+// let date = '4/1/18';
+// let venue = 'Yo Momma\'s house';
+// let time = 'All the time';
 
-document.querySelector('.djevent').innerHTML = `
-<td>${date}</td>
-<td>${venue}</td>
-<td>${time}</td>
-`
+// document.querySelector('.djevent').innerHTML = `
+// <td>${date}</td>
+// <td>${venue}</td>
+// <td>${time}</td>
+// `;
+
+////////HTML include import code////////////////
+
+let link = document.querySelector('link[rel="import"]');
+let calendar = link.import;
+let content = calendar.querySelector('table');
+let calendarDiv = document.querySelector('.calendar-div');
+
+calendarDiv.appendChild(content.cloneNode(true));
---
+++ djr8/js/carousel.js
@@ -0,0 +1,7 @@
+	// jquery for carousel
+  $('.carousel').carousel({
+    interval:2500,
+    keyboard: true,
+    pause: 'hover',
+    wrap: true
+  });
