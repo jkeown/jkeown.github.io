200b356a07916e0a0538e5ae43262bb26fc6df47
jared keown <jd.keown@yahoo.com>
2018-07-27 10:31:12 -0700

    removed orig profile and upload fcc profile

--- djr8/js/carousel.js
+++
@@ -1,7 +0,0 @@
-	// jquery for carousel
-  $('.carousel').carousel({
-    interval:2500,
-    keyboard: true,
-    pause: 'hover',
-    wrap: true
-  });
---
+++ js/app.js
@@ -0,0 +1,20 @@
+// mobile side nav
+document.addEventListener('DOMContentLoaded', function() {
+  let elems = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(elems, {edge:'right'});
+});
+
+// carousel
+document.addEventListener('DOMContentLoaded', function() {
+  let elems = document.querySelectorAll('.slider');
+  M.Slider.init(elems,{
+    interval: 4000,
+    indicators: true
+  });
+});
+
+// scrollspy
+document.addEventListener('DOMContentLoaded', function() {
+  let elems = document.querySelectorAll('.scrollspy');
+  M.ScrollSpy.init(elems,{});
+});
